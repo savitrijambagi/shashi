04719e87f5bdb5cd1f523eb6b54239492e3014ab
unknown <savitrijambagi07@gmail.com>
2025-02-14 21:31:12 +0530

    Welcome to CBKore Polytechnic

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ public class Demo
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hi welcome to CBKore Polytechnic");
+		System.out.println("Welcome to CBKore Polytechnic");
 	}
 }
