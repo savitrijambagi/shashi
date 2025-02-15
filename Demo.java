f6248920643918e7b1a22c85c1c7f7ae80054175
unknown <savitrijambagi07@gmail.com>
2025-02-15 11:14:53 +0530

    changed input string

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ public class Demo
 {
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to CBKore Polytechnic");
+		System.out.println("Welcome to C B Kore Polytechnic chikodi");
 	}
 }
