2b3b5f14f949ad91b771fbb4237350a1b944ee5e
unknown <savitrijambagi07@gmail.com>
2025-02-15 11:38:30 +0530

    Welcome to C B Kore Polytechnic chikodi rakshita

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ public class Demo
 {
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to C B Kore Polytechnic chikodi prashant");
+		System.out.println("Welcome to C B Kore Polytechnic chikodi rakshita");
 	}
 }
